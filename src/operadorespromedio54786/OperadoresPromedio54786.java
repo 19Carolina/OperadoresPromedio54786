fa9af91a26bd95e84c5dc2d7d288ea078339d94a
carol <carol@192.168.1.71>
2021-02-27 19:24:33 -0600

    cuarto proyecto java

---
+++ src/operadorespromedio54786/OperadoresPromedio54786.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadorespromedio54786;
+
+/**
+ *
+ * @author carol
+ */
+public class OperadoresPromedio54786 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+      int variable1 = 10;
+      int variable2 = 25;
+      int variable3 = 35;
+      int variable4 = 70;
+      int variable5 = 30;
+      
+      int promedio;
+      
+            
+      promedio = (variable1+variable2+variable3+variable4+variable5)/5;
+      
+      System.out.println("El promedio es:" + promedio);
+    }
+    
+}
